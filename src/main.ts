e84442c49791390a9c3795ca61e1e2ac9491ae02
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-10-26 21:16:38 +0100

    Changed class name of the surrounding div

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="grid">
+  <div class="main-grid">
     <div class="about-me">
       <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
     </div>
