806c0461ed20fa50affa1344d315c36a8998f472
Suchti18 <78668364+Suchti18@users.noreply.github.com>
2025-11-02 16:55:28 +0100

    Added a simple test project card

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import "./style.css"
+import "./main.css"
+import "./projects.css"
 
 // Skills images
 import java_logo from "/java-logo.svg"
@@ -46,7 +47,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
 
     <div class="projects">
-      <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+      <div class="project-card">
+        <div class="project-card-label">
+          <p>Title</p>
+        </div>
+        <div class="project-card-subtitle">
+          <p>Text</p>
+        </div>
+      </div>
     </div>
   </div>
 `
