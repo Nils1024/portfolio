f67911e8eba9fbaf8d0a9a42ce01336f5aff0574
Nils1024 <78668364+Suchti18@users.noreply.github.com>
2025-11-02 23:35:58 +0100

    Changed grid layout and added hover effect

--- src/main.ts
+++ src/main.ts
@@ -13,48 +13,89 @@ import SoM_logo from "/SoM-logo.png"
 import github_logo from "/github-logo.svg?raw"
 import mail_symbol from "/mail-symbol.svg?raw"
 
+const github_logo_with_gradient = github_logo;
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main-grid">
-    <div class="about-me">
-      <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
-    </div>
-
-    <div class="events">
-      <a href="https://summer.hackclub.com" target="_blank" alt="Summer of Making" title="Summer of Making">
-        <img src="${SoM_logo}">
-      </a>
-    </div>
-
-    <div class="contact">
-      <a href="https://github.com/Nils1024" target="_blank" alt="GitHub">
-        ${github_logo}
-      </a>
-      <a href="mailto:test@email.de" alt="E-Mail">
-        ${mail_symbol}
-      </a>
-    </div>
-
-    <div class="skills">
-      <a href="https://www.java.com" target="_blank">
-        <img src="${java_logo}">
-      </a>
-      <a href="https://www.c-language.org" target="_blank">
-        <img src="${c_logo}">
-      </a>
-      <a href="https://git-scm.com" target="_blank">
-        <img src="${git_logo}">
-      </a>
-    </div>
-
-    <div class="projects">
-      <div class="project-card">
-        <div class="project-card-label">
-          <p>Title</p>
+    <ul>  
+      <li class="about-me">
+        <div>
+          <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
         </div>
-        <div class="project-card-subtitle">
-          <p>Text</p>
+      </li>
+
+      <li class="events">
+        <div>
+          <a href="https://summer.hackclub.com" target="_blank" alt="Summer of Making" title="Summer of Making">
+            <img src="${SoM_logo}">
+          </a>
         </div>
-      </div>
-    </div>
+      </li>
+
+      <li class="projects">
+        <div>
+          <div class="project-card">
+            <div class="project-card-label">
+              <p>Title</p>
+            </div>
+            <div class="project-card-subtitle">
+              <p>Text</p>
+            </div>
+          </div>
+
+          <div class="project-card">
+            <div class="project-card-label">
+              <p>Title</p>
+            </div>
+            <div class="project-card-subtitle">
+              <p>Text</p>
+            </div>
+          </div>
+
+          <div class="project-card">
+            <div class="project-card-label">
+              <p>Title</p>
+            </div>
+            <div class="project-card-subtitle">
+              <p>Text</p>
+            </div>
+          </div>
+
+          <div class="project-card">
+            <div class="project-card-label">
+              <p>Title</p>
+            </div>
+            <div class="project-card-subtitle">
+              <p>Text</p>
+            </div>
+          </div>
+        </div>
+      </li>
+
+      <li class="contact">
+        <div>
+          <a href="https://github.com/Nils1024" target="_blank" alt="GitHub">
+            ${github_logo_with_gradient}
+          </a>
+          <a href="mailto:test@email.de" alt="E-Mail">
+            ${mail_symbol}
+          </a>
+        </div>
+      </li>
+
+      <li class="skills">
+        <div>
+          <a href="https://www.java.com" target="_blank">
+            <img src="${java_logo}">
+          </a>
+          <a href="https://www.c-language.org" target="_blank">
+            <img src="${c_logo}">
+          </a>
+          <a href="https://git-scm.com" target="_blank">
+            <img src="${git_logo}">
+          </a>
+        </div>
+      </li>
+    </ul>
   </div>
 `
